4e789c565343baa05419cc60fa2336df68958c1b
ElRodrigote <rodrigo.ali@nuant.com>
2024-04-28 18:54:26 +0200

    chore: remove unwanted spacing in the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { SendboxContainer } from "@/components";
 
 export default function Home() {
   return (
-    <main className="flex min-h-[calc(100vh-3.5rem)] flex-col items-center justify-between p-24 space-y-4">
+    <main className="flex min-h-[calc(100vh-3.5rem)] flex-col items-center p-24 space-y-4">
       <div>
         {`No testnet gas? No problem! use `}
         <Link
